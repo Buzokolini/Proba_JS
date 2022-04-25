ac39b11717c280efc636d20902a4a458624dc9b4
LVN <levkovich@yandex.ru>
2022-04-25 21:37:53 +0300

    01 commit

---
+++ Lessons/01/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+//console.log(second);
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// }
+
+// console.log(person.name);
+// console.log(person["name"]);
+
+// let arr = ['plan.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+//alert("Hello, World");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+
+    console.log(++incr);
+    console.log(decr--);
+
+    let isChecked = true;
+        isClose = false;
+
+    console.log(isChecked && isClose); 
+    console.log(isChecked || isClose); 
