8bb8c5f596dda56961a1ead549bd81114ae93875
LVN <levkovich@yandex.ru>
2022-04-26 16:28:47 +0300

    02 dopolnenie

---
+++ DomZadan/02_26.04.22/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appdata = {
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false,
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+    appdata.expenses.a1 = a2;
+    appdata.expenses.a3 = a4;
+
+    alert("Ваш бюджет на 1 день: " + appdata.budjet / 30 + "рублей");
+    
